17b6eb366a2064368226f8a9c981d3688e0bb281
Bryan Higgz <brian@tafutaa.com>
2021-07-30 14:13:43 +0300

    added drawer to posts page

--- src/app/Pages/posts/posts.page.ts
+++ src/app/Pages/posts/posts.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonContent } from '@ionic/angular';
+import { DrawerComponent } from 'src/app/components/drawer/drawer.component';
+import { DrawerService } from 'src/app/Services/Drawer/drawer.service';
 
 @Component({
   selector: 'app-posts',
@@ -10,16 +12,28 @@ import { IonContent } from '@ionic/angular';
 export class PostsPage implements OnInit {
 
   @ViewChild(IonContent, { static: false }) content: IonContent;
+  @ViewChild(DrawerComponent) drawer: DrawerComponent;
   user: any;
   images: any;
   id: any;
   nimages: any;
+  backdropVisible = false;
 
-  constructor(private route: ActivatedRoute,) {
+  constructor(private route: ActivatedRoute,
+    private changeDetectorRef: ChangeDetectorRef,
+    private drawerService: DrawerService,
+
+    ) {
     this.user = JSON.parse(this.route.snapshot.paramMap.get('user')) || 0;
     this.nimages = JSON.parse(this.route.snapshot.paramMap.get('images')) || 0;
     this.id = JSON.parse(this.route.snapshot.paramMap.get('selectedId')) || 0;
     this.topImage();
+
+    this.drawerService.drawerOpen.subscribe(drawerData => {
+      if (drawerData && drawerData.open) {
+        this.drawer.openDrawer();
+      }
+    })
 
   }
 
@@ -34,6 +48,15 @@ export class PostsPage implements OnInit {
     var first = this.id;
     this.images = this.nimages.sort(function (x, y) { return x.id == first ? -1 : y.id == first ? 1 : 0; });
     // console.log('new position',this.nimages);
+  }
+
+  toggleBackdrop(isVisible) {
+    this.backdropVisible = isVisible;
+    this.changeDetectorRef.detectChanges();
+  }
+
+  closeDrawer() {
+    this.drawer.closeDrawer();
   }
 
 
